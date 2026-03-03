e95aac21f1880f0f9abc3694ff1c6842b79be50b
printcess <pngerdchai@gmail.com>
2026-03-03 17:39:17 +0700

    adding reservation

--- server.js
+++ server.js
@@ -1,66 +1,66 @@
 const { setServers } = require("node:dns/promises");
 setServers(["1.1.1.1", "8.8.8.8"]);
 
-const swaggerJsDoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
-const express = require('express');
-const dotenv = require('dotenv');
-const cookieParser = require('cookie-parser');
-const connectDB = require('./config/db');
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const express = require("express");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const connectDB = require("../Pushing/Nei/config/db");
 
-const swaggerOptions={
-    swaggerDefinition:{
-        openapi: '3.0.0',
-        info: {
-            title: 'Library API',
-            version: '1.0.0',
-            description: 'A simple Express e-ses API'
-        },
-        servers:[
-            {
-                url: 'http://localhost:5000/api/v1'
-            }
-        ],
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Library API",
+      version: "1.0.0",
+      description: "A simple Express e-ses API",
     },
-    apis: ['./docs/*.js']
+    servers: [
+      {
+        url: "http://localhost:5000/api/v1",
+      },
+    ],
+  },
+  apis: ["./docs/*.js"],
 };
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 // Load env vars
-dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: "./config/config.env" });
 
 // Connect to database
 connectDB();
 
 // Route files
-const coworkingspaces = require('./routes/coworkingspaces');
-const auth = require('./routes/auth');
+const coworkingspaces = require("./routes/coworkingspaces");
+const reservations = require("./routes/reservations");
+const auth = require("./routes/auth");
 
 const app = express();
 
-app.set('query parser', 'extended');
+app.set("query parser", "extended");
 
 // Body parser
 app.use(express.json());
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 // Cookie parser
 app.use(cookieParser());
 
 // Mount routers
-app.use('/api/v1/coworkingspaces', coworkingspaces);
-app.use('/api/v1/auth', auth);
+app.use("/api/v1/coworkingspaces", coworkingspaces);
+app.use("/api/v1/reservations", reservations);
+app.use("/api/v1/auth", auth);
 
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
-  console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
-  )
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`),
 );
 
 // Handle unhandled promise rejections
-process.on('unhandledRejection', (err) => {
+process.on("unhandledRejection", (err) => {
   console.log(`Error: ${err.message}`);
   server.close(() => process.exit(1));
 });
